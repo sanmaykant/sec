d08d79f5bbf4acd74fc4d33b256e018b2cdd74c3
sanmay <sanmaykant1@gmail.com>
2026-04-17 19:54:39 +0530

    Switched to lazy loading embedding model to remove loading redundant copies across workers

--- app/tasks/embedding_tasks.py
+++ app/tasks/embedding_tasks.py
@@ -10,10 +10,19 @@ from app.utils.preprocessing import preprocess_text, chunk_text
 import os
 os.environ["HF_HUB_OFFLINE"] = "1"
 
-model = SentenceTransformer(
-        "sentence-transformers/all-mpnet-base-v2",
-        revision="e8c3b32edf5434bc2275fc9bab85f82640a19130",
-        local_files_only=True, device="cuda")
+_model = None  # Global placeholder
+
+def get_model():
+    global _model
+    if _model is None:
+        logger.info("Loading model into VRAM for the first time...")
+        _model = SentenceTransformer(
+            "sentence-transformers/all-mpnet-base-v2",
+            revision="e8c3b32edf5434bc2275fc9bab85f82640a19130",
+            local_files_only=True, 
+            device="cuda"
+        )
+    return _model
 
 @celery_app.task(
     queue="embed_queue",
@@ -24,6 +33,8 @@ model = SentenceTransformer(
 def embed_text(ctx):
     if ctx.get("status") != "success":
         return ctx
+
+    model = get_model()
 
     logger.info("Processing text")
 
@@ -38,11 +49,14 @@ def embed_text(ctx):
     embeddings = model.encode(chunks, convert_to_tensor=True, batch_size=32)
     logger.info(f"Embedded text {embeddings}")
 
-    ctx["embeddings"] = embeddings.tolist()
+    embeddings_cpu = embeddings.detach().cpu().numpy()
+    del embeddings 
+
+    ctx["embeddings"] = embeddings_cpu.tolist()
     ctx["chunks"] = chunks
 
 # Explicitly cleanup
-    del embeddings 
+    del embeddings_cpu
     gc.collect() # Trigger Python garbage collection
     if torch.cuda.is_available():
         torch.cuda.empty_cache() # Release unused VRAM back to the GPU
